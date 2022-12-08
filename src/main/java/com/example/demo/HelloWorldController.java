52c8f2b9356c10f8f392642eaebb829c3ef72a69
AquatiCarnivore <109682769+AquatiCarnivore@users.noreply.github.com>
2022-12-08 12:35:12 +0200

    Nu imi afiseaza data, sigur nu fac ceva bine, am incercat in mai multe feluri, niciuna nu voia sa afiseze data.

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -4,20 +4,26 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 @RestController
 public class HelloWorldController {
 
-	@GetMapping("/")
-	public String sayHi() {
-		return "Hello world";
-	}
+//	@GetMapping("/")
+//	public String sayHi() {
+//		return "Hello world";
+//	}
 
-	@GetMapping("/date")
-	public Date date(@RequestParam("date")
-					 @DateTimeFormat(pattern = "dd.MM.yyyy") Date date) { return date;}
+	@GetMapping("/")
+	public String dateFormat(){
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		Date date = new Date();
+		return formatter.format(date);
 	}
+}
 
 
 
